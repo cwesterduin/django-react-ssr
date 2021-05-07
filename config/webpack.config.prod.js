a2f5862b458929d0f46b44fceaabee61da62558e
chris westerduin <chriswesterduin@outlook.com>
2021-05-07 16:43:42 +0100

    add production commands and change buildpack

---
+++ config/webpack.config.prod.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+
+var config = require('./webpack.config.js')
+
+config.output.path = require('path').resolve('./src/dist')
+
+config.plugins = config.plugins.concat([
+  new BundleTracker({filename: './webpack-stats-prod.json'}),
+
+  // removes a lot of debugging code in React
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+  }}),
+
+  // keeps hashes consistent between compilations
+  // new webpack.optimize.OccurenceOrderPlugin(),
+
+  // minifies your code
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compressor: {
+  //     warnings: false
+  //   }
+  // })
+])
+
+
+module.exports = config
