bc329beec7259eb3b37630d59f7a06cd708403e6
chris westerduin <chriswesterduin@outlook.com>
2021-05-07 22:16:56 +0100

    update static files

--- library/settings.py
+++ library/settings.py
@@ -32,8 +32,14 @@ DEBUG = False
 
 ALLOWED_HOSTS = []
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'src'), # We do this so that django's collectstatic copies or our bundles to the STATIC_ROOT or syncs them to whatever storage we use.
+    os.path.join(BASE_DIR, 'src'),
 )
 
 WEBPACK_LOADER = {
